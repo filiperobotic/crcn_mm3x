2941c7819e02c70950224ce2515fd4842530873b
filiperobotic <filipe.rolim@gmail.com>
2025-04-15 13:44:24 -0300

    ok

--- configs/my_config/models/atts.py
+++ configs/my_config/models/atts.py
@@ -61,6 +61,12 @@ model = dict(
         score_thr=0.05,
         nms=dict(type='nms', iou_threshold=0.6),
         max_per_img=100))
+    val_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.6),
+        max_per_img=100))
 # optimizer
 # optim_wrapper = dict(
 #     optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
