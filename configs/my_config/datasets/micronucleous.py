630d5b9f7d8e70d4b8f63acf3bd58b8f1db11d7a
filiperobotic <filipe.rolim@gmail.com>
2025-04-15 12:09:24 -0300

    anchors

--- configs/my_config/datasets/micronucleous.py
+++ configs/my_config/datasets/micronucleous.py
@@ -23,8 +23,8 @@ backend_args = None
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args=backend_args),
     dict(type='LoadAnnotations', with_bbox=True),
-    # dict(type='Resize', scale=(1000, 600), keep_ratio=True),
-    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='Resize', scale=(1000, 600), keep_ratio=True),
+    # dict(type='Resize', scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', prob=0.5), #debug
     dict(type='PackDetInputs')
 ]
