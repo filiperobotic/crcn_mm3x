6f6792af01e2a441436ca921ceb600a4afe45750
filiperobotic <filipe.rolim@gmail.com>
2025-04-15 13:48:50 -0300

    ok

--- configs/my_config/sota/micronucleo_atts_1x_voc0712_20c_12x1ep.py
+++ configs/my_config/sota/micronucleo_atts_1x_voc0712_20c_12x1ep.py
@@ -20,28 +20,12 @@ model = dict(
 # `_base_/datasets/voc0712.py`, so the actual epoch = 4 * 3 = 12
 #max_epochs = 12
 max_epochs = 50
-# train_cfg = dict(
-#     type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
-# val_cfg = dict(type='ValLoop')
-# test_cfg = dict(type='TestLoop')
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
 
-train_cfg=dict(
-        # assigner=dict(type='ATSSAssigner', topk=9),
-        # allowed_border=-1,
-        # pos_weight=-1,
-        debug=False)
-test_cfg=dict(
-        nms_pre=1000,
-        min_bbox_size=0,
-        score_thr=0.05,
-        nms=dict(type='nms', iou_threshold=0.6),
-        max_per_img=100),
-val_cfg=dict(
-        nms_pre=1000,
-        min_bbox_size=0,
-        score_thr=0.05,
-        nms=dict(type='nms', iou_threshold=0.6),
-        max_per_img=100)
+
 
 # learning rate
 param_scheduler = [
