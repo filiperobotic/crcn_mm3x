3ed52624c3e7d36404298d7b952cff1f2e2199a9
filiperobotic <filipe.rolim@gmail.com>
2025-04-15 14:19:43 -0300

    ij

--- configs/my_config/models/retinanet.py
+++ configs/my_config/models/retinanet.py
@@ -35,14 +35,13 @@ model = dict(
             #octave_base_scale=4,
             octave_base_scale=2,
             scales_per_octave=3,
-            
             ratios=[0.5, 1.0, 2.0],
             strides=[8, 16, 32, 64, 128]),
         bbox_coder=dict(
             type='DeltaXYWHBBoxCoder',
             target_means=[.0, .0, .0, .0],
             # target_stds=[1.0, 1.0, 1.0, 1.0]),
-            target_stds=[0.1, 0.1, 0.2, 0.2],
+            target_stds=[0.1, 0.1, 0.2, 0.2]),
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
