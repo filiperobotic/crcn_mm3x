1c94de1393326146b247ad17ededba5804157fc6
filiperobotic <filipe.rolim@gmail.com>
2025-05-29 10:48:32 -0300

    ok

--- configs/my_config/models/detr.py
+++ configs/my_config/models/detr.py
@@ -86,40 +86,40 @@ model = dict(
 
 # # train_pipeline, NOTE the img_scale and the Pad's size_divisor is different
 # # from the default setting in mmdet.
-train_pipeline = [
-    #dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
-    dict(type='LoadImageFromFile', backend_args=None),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='RandomFlip', prob=0.5),
-    dict(
-        type='RandomChoice',
-        transforms=[[
-            dict(
-                type='RandomChoiceResize',
-                scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
-                        (608, 1333), (640, 1333), (672, 1333), (704, 1333),
-                        (736, 1333), (768, 1333), (800, 1333)],
-                keep_ratio=True)
-        ],
-                    [
-                        dict(
-                            type='RandomChoiceResize',
-                            scales=[(400, 1333), (500, 1333), (600, 1333)],
-                            keep_ratio=True),
-                        dict(
-                            type='RandomCrop',
-                            crop_type='absolute_range',
-                            crop_size=(384, 600),
-                            allow_negative_crop=True),
-                        dict(
-                            type='RandomChoiceResize',
-                            scales=[(480, 1333), (512, 1333), (544, 1333),
-                                    (576, 1333), (608, 1333), (640, 1333),
-                                    (672, 1333), (704, 1333), (736, 1333),
-                                    (768, 1333), (800, 1333)],
-                            keep_ratio=True)
-                    ]]),
-    dict(type='PackDetInputs')
-]
-train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+# train_pipeline = [
+#     #dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
+#     dict(type='LoadImageFromFile', backend_args=None),
+#     dict(type='LoadAnnotations', with_bbox=True),
+#     dict(type='RandomFlip', prob=0.5),
+#     dict(
+#         type='RandomChoice',
+#         transforms=[[
+#             dict(
+#                 type='RandomChoiceResize',
+#                 scales=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+#                         (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+#                         (736, 1333), (768, 1333), (800, 1333)],
+#                 keep_ratio=True)
+#         ],
+#                     [
+#                         dict(
+#                             type='RandomChoiceResize',
+#                             scales=[(400, 1333), (500, 1333), (600, 1333)],
+#                             keep_ratio=True),
+#                         dict(
+#                             type='RandomCrop',
+#                             crop_type='absolute_range',
+#                             crop_size=(384, 600),
+#                             allow_negative_crop=True),
+#                         dict(
+#                             type='RandomChoiceResize',
+#                             scales=[(480, 1333), (512, 1333), (544, 1333),
+#                                     (576, 1333), (608, 1333), (640, 1333),
+#                                     (672, 1333), (704, 1333), (736, 1333),
+#                                     (768, 1333), (800, 1333)],
+#                             keep_ratio=True)
+#                     ]]),
+#     dict(type='PackDetInputs')
+# ]
+# train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
 
